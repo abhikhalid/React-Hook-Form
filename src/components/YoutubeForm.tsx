57abde06a4552a06476a73534c617e01188ab1a4
kmahmud <kmahmud@csafeglobal.com>
2024-10-24 20:30:29 +0600

    Form Validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,8 +1,6 @@
 import {useForm} from 'react-hook-form';
 import {DevTool} from '@hookform/devtools';
 
-let renderCount = 0;
-
 
 type FormValues = {
     username: string;
@@ -11,39 +9,38 @@ type FormValues = {
 }
 
 export default function YoutubeForm() {
- //useForm is a custom hook that returns an object with methods and properties that we can use to build our form; for managing 
- // i. Manage form data ii. Submit form data. iii. Enforce validations and provide visual feedback   
+
  const form =  useForm<FormValues>();
 
  const {register, control, handleSubmit} = form;
-
-//  const {name, ref, onChange, onBlur} = register('username');
 
   const onSubmit = (data: FormValues) => {
     console.log('Form Submitted', data);
   }
 
-
   return (
     <div>
-        {/* When I type into the form fields,  this component does not re-render */}
-        {/* This is great for performance. With traditional  react forms, when you work with controlled components, every keystroke will cause the component and it's children to re-render */}
-        {/* React hook form on the other hand does not do that. It follows the uncontrolled input behavior. */}
-        <h1>Youtube Form ({renderCount/2})</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
+      {/* noValidate : this will prevent browser validation allowing react hook form to handle the validation of the fields*/}
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
             <label htmlFor="username">Username</label>
             <input 
                 type="text" 
                 id="username" 
-                // name={name} 
-                // ref={ref} 
-                // onChange={onChange}
-                // onBlur={onBlur} 
-                {...register('username')}
+                {...register('username',{
+                  required: {
+                    value: true,
+                    message: "UserName is required",
+                  }}
+                 )}
              />
 
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" {...register('email')}/>
+            <input type="email" id="email" {...register('email',{
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Invalid email address"
+              }
+            })}/>
 
             <label htmlFor="channel">Channel</label>
             <input type="text"  id="channel" {...register('channel')} />
