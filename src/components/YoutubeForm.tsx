5e515cb11652ce1e0fa3e38a7d3864104549e18a
kmahmud <kmahmud@csafeglobal.com>
2024-10-24 21:05:13 +0600

    Display Error Messages

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -12,7 +12,9 @@ export default function YoutubeForm() {
 
  const form =  useForm<FormValues>();
 
- const {register, control, handleSubmit} = form;
+ const {register, control, handleSubmit, formState} = form;
+
+ const {errors} = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log('Form Submitted', data);
@@ -22,28 +24,38 @@ export default function YoutubeForm() {
     <div>
       {/* noValidate : this will prevent browser validation allowing react hook form to handle the validation of the fields*/}
         <form onSubmit={handleSubmit(onSubmit)} noValidate>
-            <label htmlFor="username">Username</label>
-            <input 
-                type="text" 
-                id="username" 
-                {...register('username',{
-                  required: {
-                    value: true,
-                    message: "UserName is required",
-                  }}
-                 )}
-             />
+            <div className='form-control'>
+              <label htmlFor="username">Username</label>
+              <input 
+                  type="text" 
+                  id="username" 
+                  {...register('username',{
+                    required: {
+                      value: true,
+                      message: "Username is required",
+                    }}
+                  )}
+              />
+             <p className='error'>{errors.username?.message}</p>
+            </div>
 
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" {...register('email',{
-              pattern: {
-                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                message: "Invalid email address"
-              }
-            })}/>
+            <div className='form-control'>
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" {...register('email',{
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email address"
+                }
+              })}/>
 
-            <label htmlFor="channel">Channel</label>
-            <input type="text"  id="channel" {...register('channel')} />
+              <p className='error'>{errors.email?.message}</p>
+            </div>
+
+            <div className='form-control'>  
+              <label htmlFor="channel">Channel</label>
+              <input type="text"  id="channel" {...register('channel')} />
+              <p className='error'>{errors.channel?.message}</p>
+            </div>
 
             <button>Submit</button>
         </form>
